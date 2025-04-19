dfeaa5470aeca7cedc47c615bfecd3ef66f5a637
vikas <vikassharma26543@gmail.com>
2025-04-19 20:43:03 +0530

    project frontend part setup done

---
+++ react-chat-app/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Button} from "../src/components/ui/button"
+
+const App = () => {
+  return (
+    <div>
+      <Button>Click Me</Button>
+    </div>
+  )
+}
+
+export default App
