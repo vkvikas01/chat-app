40afe4df73ec22756a9bc86d486b1bab22890cd2
vikas <vikassharma26543@gmail.com>
2025-04-28 21:49:39 +0530

    auth api done

--- server/controllers/AuthController.js
+++ server/controllers/AuthController.js
@@ -1,34 +1,111 @@
-
 import jwt from "jsonwebtoken";
 import User from "../models/UserModel.js";
-const maxAge=3*24860*6081000;
-const createToken=(email,userId)=>{
-    return jwt.sign({email,userId},process.env.JWT_KEY,{expiresIn:maxAge})
-}
-export const signup=async(request,response,next)=>{
-    try{
-        const {email,password}=request.body;
-        if(!email||!password){
-            return response.status(400).send("Email and password required");
-        }
-        const user =await User.create({email,password});
-        response.cookie("jwt",createToken(email,user.id),{
-            maxAge,
-            secure:true,
-            sameSite:"None"
-        })
-        return response.status(201).json({user:{
-            user:{
-                id:user.id,
-            email:user.email,
-            // firstName:user.firstName,
-            // lastName:user.lastName,
-            // image:user.image,
-            profileSetup:user.profileSetup,
-            }
-        }})
-    }catch(error){
-        console.log({error});
-        return response.status(500).send("Internal Server Error");
+import { compare } from "bcrypt";
+const maxAge = 3 * 24860 * 6081000;
+const createToken = (email, userId) => {
+  return jwt.sign({ email, userId }, process.env.JWT_KEY, {
+    expiresIn: maxAge,
+  });
+};
+export const signup = async (request, response, next) => {
+  try {
+    const { email, password } = request.body;
+    if (!email || !password) {
+      return response.status(400).send("Email and password required");
     }
-}
+    const user = await User.create({ email, password });
+    response.cookie("jwt", createToken(email, user.id), {
+      maxAge,
+      secure: true,
+      sameSite: "None",
+    });
+    return response.status(201).json({
+      user: {
+        user: {
+          id: user.id,
+          email: user.email,
+          // firstName:user.firstName,
+          // lastName:user.lastName,
+          // image:user.image,
+          profileSetup: user.profileSetup,
+        },
+      },
+    });
+  } catch (error) {
+    console.log({ error });
+    return response.status(500).send("Internal Server Error");
+  }
+};
+
+export const login = async (request, response, next) => {
+  try {
+    const { email, password } = request.body;
+    if (!email || !password) {
+      return response.status(400).send("Email and password required");
+    }
+    const user = await User.findOne({ email });
+    if (!user) {
+      return response.status(404).send("User Not Found");
+    }
+    const auth = await compare(password, user.password);
+    if (!auth) {
+      return response.status(404).send("password is incorrect");
+    }
+    response.cookie("jwt", createToken(email, user.id), {
+      maxAge,
+      secure: true,
+      sameSite: "None",
+    });
+    return response.status(200).json({
+      user: {
+        user: {
+          id: user.id,
+          email: user.email,
+          // firstName:user.firstName,
+          // lastName:user.lastName,
+          // image:user.image,
+          profileSetup: user.profileSetup,
+          firstName: user.firstName,
+          lastName: user.lastName,
+          image: user.image,
+          color: user.color,
+        },
+      },
+    });
+  } catch (error) {
+    console.log({ error });
+    return response.status(500).send("Internal Server Error");
+  }
+};
+
+export const getUserInfo=async (request, response, next) => {
+  try {
+    
+    const userData= await User.findById(request.userId);
+    if(!userData){
+      return response.status(404).send("User with  the given id Not Found");
+    }
+    // response.cookie("jwt", createToken(email, user.id), {
+    //   maxAge,
+    //   secure: true,
+    //   sameSite: "None",
+    // });
+    return response.status(200).json({
+      
+          id: userData.id,
+          email: userData.email,
+          // firstName:user.firstName,
+          // lastName:user.lastName,
+          // image:user.image,
+          profileSetup: userData.profileSetup,
+          firstName: userData.firstName,
+          lastName: userData.lastName,
+          image: userData.image,
+          color: userData.color,
+      
+    });
+  } catch (error) {
+    console.log({ error });
+    return response.status(500).send("Internal Server Error");
+  }
+};
