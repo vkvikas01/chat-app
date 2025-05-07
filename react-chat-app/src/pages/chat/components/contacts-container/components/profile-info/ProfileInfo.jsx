b61aa59078df7eb773aabaa2de73c61ce7bee65f
vikas <vikassharma26543@gmail.com>
2025-05-07 21:26:24 +0530

    logout functionality done

--- react-chat-app/src/pages/chat/components/contacts-container/components/profile-info/ProfileInfo.jsx
+++ react-chat-app/src/pages/chat/components/contacts-container/components/profile-info/ProfileInfo.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 import { useAppStore } from '../../../../../../store'
 import { getColor } from '../../../../../../lib/utils';
-import { HOST } from '../../../../../../utils/constants';
+import { HOST, LOGOUT_ROUTE } from '../../../../../../utils/constants';
 import {
     Tooltip,
     TooltipContent,
@@ -10,10 +10,25 @@ import {
     TooltipTrigger,
 } from "@/components/ui/tooltip"
 import {FiEdit2} from "react-icons/fi"
+import {IoPowerSharp} from "react-icons/io5"
+import {  useNavigate } from 'react-router-dom';
+import { apiClient } from '../../../../../../lib/api-client';
 
 
 function ProfileInfo() {
-    const { userInfo } = useAppStore();
+    const navigate=useNavigate();
+    const { userInfo,setUserInfo } = useAppStore();
+    const logOut=async()=>{
+        try {
+            const response=await apiClient.post(LOGOUT_ROUTE,{},{withCredentials:true})
+            if(response.status===200){
+                navigate('/auth');
+                setUserInfo(null)
+            }
+        } catch (error) {
+            console.log(error)
+        }
+    }
     return (
         <div className='absolute bottom-0 h-16 flex items-center justify-between px-10 w-full bg-[#2a2b33]' >
             <div className="flex gap-3 items-center justify-center">
@@ -36,9 +51,17 @@ function ProfileInfo() {
             <div className="flex gap-5">
                 <TooltipProvider>
                     <Tooltip>
-                        <TooltipTrigger><FiEdit2 className="text-purple-500 text-xl font-medium"/></TooltipTrigger>
-                        <TooltipContent className="bg-[#1c1b1e]">
+                        <TooltipTrigger><FiEdit2 className="text-purple-500 text-xl font-medium cursor-pointer" onClick={()=>navigate('/profile')}/></TooltipTrigger>
+                        <TooltipContent className="bg-[#1c1b1e] border-none text-white">
                             Edit Profile
+                        </TooltipContent>
+                    </Tooltip>
+                </TooltipProvider>
+                <TooltipProvider>
+                    <Tooltip>
+                        <TooltipTrigger><IoPowerSharp className="text-red-500 text-xl font-medium cursor-pointer" onClick={()=>logOut()}/></TooltipTrigger>
+                        <TooltipContent className="bg-[#1c1b1e] border-none text-white">
+                            Logout
                         </TooltipContent>
                     </Tooltip>
                 </TooltipProvider>
